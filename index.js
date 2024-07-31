cf9d9f57c2c1cbddafc280c87f8e2078736fbbea
Atharva <chaudhariatharva041@gmail.com>
2024-07-31 08:14:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/atharva',(req,res)=>{
+  res.send('this is atharva here')
+})
+app.get('/htmltry', (req,res)=>{
+  res.send("<H1>THis is the h1 tag</H1>")
+})
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
