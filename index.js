d2c9ad4d0845046467f59a8d437f0e25daf86914
Atharva <chaudhariatharva041@gmail.com>
2024-07-31 08:38:02 +0530

    change

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get('/atharva',(req,res)=>{
 app.get('/htmltry', (req,res)=>{
   res.send("<H1>THis is the h1 tag</H1>")
 })
+
+app.get('/github',(req,res)=>{
+  res.send("https://github.com/atharvachaudhari")
+})
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
